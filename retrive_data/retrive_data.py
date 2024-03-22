03e43f9a3500cb19e77da2ffaee3420501f470ee
Kasperi <kasperi.ervasti@student.oulu.fi>
2024-03-22 20:15:43 +0200

    Made changes to retrive_data.py

--- retrive_data/retrive_data.py
+++ retrive_data/retrive_data.py
@@ -1,94 +1,131 @@
 import requests
 import json
+from datetime import datetime, timedelta
+from dataclasses import dataclass
+
+from typing import List
 
 
-def get_json_info(json_data):
+@dataclass
+class Location:
+    location_id: int
+    location_name: str
+    person_entries: int
+    epoch_timestamp: int
+    day_of_the_week: str
+
+    def get_finnish_time(self):
+        """
+        Converts the epoch timestamp to Finnish time zone (EET: UTC+2) and returns it in a formatted string.
+
+        Returns:
+            str: A string representing the given epoch timestamp converted to Finnish time
+                in the format 'DD-MM-YYYY HH:MM:SS EET'.
+        """
+        utc_datetime = datetime.fromtimestamp(self.epoch_timestamp)
+
+        finnish_time_offset = timedelta(hours=2)  # Finland (EET: UTC+2)
+
+        finnish_datetime = utc_datetime + finnish_time_offset
+
+        formatted_finnish_time = finnish_datetime.strftime("%d-%m-%Y %H:%M:%S EET")
+
+        return formatted_finnish_time
+
+
+def get_data() -> requests.models.Response:
     """
-    Extracts information from JSON data and returns a list containing dictionaries
-    representing each entry along with the total entries.
-
-    Parameters:
-        json_data (str): A JSON formatted string containing the data to be processed.
+    Retrieves data from a specific URL and returns the response object.
 
     Returns:
-        list: A list containing dictionaries representing each entry along with the total entries.
-              Each dictionary contains the following keys:
-              - 'location_id': The ID of the location.
-              - 'location_name': The name of the location.
-              - 'person_entries': The number of person entries for the location.
-              The last dictionary in the list contains the total entries with the key 'entries_total'.
+        requests.models.Response: The response object containing the data retrieved from the URL.
 
-    Example:
-        json_data = '''
-        {
-            "entriesByLocation": [
-                {
-                    "locationId": 1,
-                    "locationName": "FUN Oulu Ritaharju",
-                    "personEntries": 21
-                }
-            ],
-            "entriesTotal": 21
-        }
-        '''
-        information = get_json_info(json_data)
-        print(information)
-
-        Output:
-        [{'location_id': 1, 'location_name': 'FUN Oulu Ritaharju', 'person_entries': 21}, {'entries_total': 21}]
     """
-    parsed_data = json.loads(json_data)
+    url = "https://funouluritaharju.fi/?controller=ajax&getentriescount=1&locationId=1"
 
+    response = requests.get(url)
+
+    return response
+
+
+def parse_locations_data(response: requests.models.Response) -> List[Location]:
+    """
+    Parses location data from the response object and returns a list of Location objects.
+
+    Parameters:
+        response (requests.models.Response): The response object containing location data.
+
+    Returns:
+        list: A list of Location objects, each representing a location entry with parsed data.
+    """
+    locations = []
+
+    date = response.headers.get("date")
+    time_object = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %Z")
+
+    epoch = int(time_object.timestamp())
+    day_of_the_week = time_object.strftime("%A")
+
+    parsed_data = json.loads(response.text)
     entries_by_location = parsed_data["entriesByLocation"]
-    entries_total = parsed_data["entriesTotal"]
-
-    info_list = []
 
     for entry in entries_by_location:
-        location_id = entry["locationId"]
-        location_name = entry["locationName"]
-        person_entries = entry["personEntries"]
+        location = Location(
+            location_id=entry["locationId"],
+            location_name=entry["locationName"],
+            person_entries=entry["personEntries"],
+            epoch_timestamp=epoch,
+            day_of_the_week=day_of_the_week,
+        )
+        locations.append(location)
 
-        entry_info = {
-            "location_id": location_id,
-            "location_name": location_name,
-            "person_entries": person_entries,
-        }
-        info_list.append(entry_info)
+    return locations
 
-    info_list.append({"entries_total": entries_total})
 
-    return info_list
+def epoch_to_finnish_time(epoch):
+    """
+    Converts the given epoch timestamp to Finnish time zone (EET: UTC+2) and returns it in a formatted string.
+
+    Parameters:
+        epoch (int): The epoch timestamp to be converted to Finnish time.
+
+    Returns:
+        str: A string representing the given epoch timestamp converted to Finnish time
+             in the format 'DD-MM-YYYY HH:MM:SS EET'.
+    """
+
+    utc_datetime = datetime.fromtimestamp(epoch)
+
+    finnish_time_offset = timedelta(hours=2)  # Finland (EET: UTC+2)
+
+    finnish_datetime = utc_datetime + finnish_time_offset
+
+    formatted_finnish_time = finnish_datetime.strftime("%d-%m-%Y %H:%M:%S EET")
+
+    return formatted_finnish_time
 
 
 if __name__ == "__main__":
 
-    url = "https://funouluritaharju.fi/?controller=ajax&getentriescount=1&locationId=1"
+    html = get_data()
 
-    page = requests.get(url)
+    # locations: List[Location] = parse_locations_data(html)
+    locations = parse_locations_data(html)
 
-    date = page.headers.get("date")
+    print()
+    print("All locations: ")
+    print(locations)
 
-    data = json.loads(page.text)
+    epoch = 0
 
-    location_id = 0
-    location_name = "No name"
-    person_entries = 0
-    entries_total = data["entriesTotal"]
+    for location in locations:
+        if location.location_id == 1:
+            epoch = location.epoch_timestamp
+            break
+    print("epoch: " + str(epoch))
 
-    entries_by_location = data["entriesByLocation"]
+    time2 = location.get_finnish_time()
+    print("Finnish time from epoch (using class function): ", time2)
 
-    for entry in entries_by_location:
-        location_id = entry["locationId"]
-        location_name = entry["locationName"]
-        person_entries = entry["personEntries"]
-        print("Location ID:", location_id)
-        print("Location Name:", location_name)
-        print("Person Entries:", person_entries)
-
-    print("Total Entries: ", entries_total)
-    jep = get_json_info(page.text)
-
-    jep.append({"date": date})
-
-    int = 2
+    time = epoch_to_finnish_time(location.epoch_timestamp)
+    print("Back to Finnish time from epoch(using function): " + time)
