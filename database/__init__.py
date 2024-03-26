e555d7d966071c4129893076f031897f1d9dc584
Kasperi <kasperi.ervasti@student.oulu.fi>
2024-03-26 20:02:41 +0200

    Fixed database/__init__.py and collected more data into visitorTrackingDB.db

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,5 @@
-from .database import *
+# __all__ = ["database"]
+
+from .db_manager import SQLiteDBManager
+from .old_db_manager import initialize_database, add_data, add_location, add_visitor_activity, get_activity_between, get_all
+
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import time
 import re
 
-
 import retrive_data
 import database
-
-
-
 
 
 def main():
@@ -16,13 +12,9 @@ def main():
     # print("Database intialized...")
     # print()
 
-    tuple_test = (1)
+    tuple_test = 1
 
-    print(retrive_data.epoch_to_finnish_time(1711128747))
-    print(retrive_data.epoch_to_finnish_time(1711212166))
-
-    # tuple_test.
-
+    print(retrive_data.epoch_to_finnish_time(1711467662))
 
     day = "No day"
     epoch = 0
@@ -32,55 +24,7 @@ def main():
 
     locationTest = retrive_data.Location(1, "nimi", 1000, 22)
 
-
-    time.sleep(0)
-
-    loca = "321321"
-    start = 131
-    end = -1
-
-    
-    if start < 0 or end < 0:
-        print("liian pieni")
-
-    if not isinstance(loca, int) or not isinstance(start, int) or not isinstance(end, int):
-        print("ei int")
-    else:
-        print("int")
-
-    # print(isinstance(12314, int))
-
-    # print(retrive_data.epoch_to_finnish_time(1711200876))
-
     juup = 2
-    # for i in range(1, 1441):
-    #     print("iteration: " + str(i))
-
-    #     html = retrive_data.get_data()
-    #     locations = retrive_data.parse_locations_data(html)
-
-    #     for location in locations:
-    #         day = location.day_of_the_week
-    #         epoch = location.epoch_timestamp
-    #         location_id = location.location_id
-    #         location_name = location.location_name
-    #         location_visitors = location.location_visitors
-
-    #     database.initialize_database()
-
-    #     database.add_to_table(epoch, location_name, person_entries, day)
-    #     print("added to table")
-    #     time.sleep(60)
-
-
-
-    # cursor.execute("SELECT * FROM fun_oulu_ritaharju")
-
-    # items = cursor.fetchall()
-    # print(items)
-
-    # for item in items:
-    #     print(item)
 
 
 if __name__ == "__main__":
