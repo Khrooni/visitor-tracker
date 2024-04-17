3462c0e22b7541a9b81f4f16c0f84cb8d4afcca9
Kasperi <kasperi.ervasti@gmail.com>
2024-04-18 01:24:21 +0300

    more helper functions. #2

--- src/database/helpers.py
+++ src/database/helpers.py
@@ -1,11 +1,35 @@
 import re
 import string
 from typing import List
-from datetime import datetime
+from datetime import datetime, timedelta
 import utils
 
+import pytz
+import math
 
 
+
+def calculate_averages(sums: list[int], counts: list[int]) -> list[float]:
+    if len(sums) != len(counts):
+        raise ValueError("The lengths of 'sums' and 'counts' lists must be the same.")
+
+    averages = [sum_val / count_val if count_val != 0 else 0.0 for sum_val, count_val in zip(sums, counts)]
+
+    return averages
+
+
+def get_timestamps(start: int, end: int, interval: int) -> List[int]:
+    """Return all timestamps of intervals that fit into start and end"""
+
+    timestamps = []
+
+    duration = end - start
+    loops = math.floor(duration / interval)
+
+    for i in range(loops):
+        timestamps.append(start + i * interval)
+
+    return timestamps
 
 def are_ints(*args):
     """True if all given arguments were of type int. False otherwise."""
@@ -14,13 +38,44 @@ def are_ints(*args):
             return False
     return True
 
-def calculate_days(start: int, end: int, weekday:str):
-    next_weekday = utils.next_weekday(start, weekday)
-    previous_weekday = utils.previous_weekday(end, weekday)
+
+def calculate_days(
+    start: int, end: int, weekday: str, tzinfo=pytz.timezone("Europe/Helsinki")
+) -> List[tuple[int, int]]:
+    """Calculates the start day (inclusive) and end day (exclusive)"""
+    weekdays: List[tuple[int, int]] = []
+
+    lower_limit = utils.lower_limit(start, weekday)  # Beginning of first weekday
+    upper_limit = utils.upper_limit(end, weekday)  # End of last weekday
+
+    while True:
+        start_day = utils.get_localized_datetime(lower_limit).replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
+        end_day = (
+            (start_day + timedelta(days=1))  # + 1 day
+            .astimezone(tzinfo)  # + timedelta doesn't check if timezone changes
+            .replace(hour=0, minute=0, second=0, microsecond=0)
+        )
+        weekdays.append(
+            (utils.datetime_to_epoch(start_day), utils.datetime_to_epoch(end_day))
+        )
+
+        lower_limit = utils.datetime_to_epoch(
+            (start_day + timedelta(weeks=1))  # + 1 week
+            .astimezone(tzinfo)  # + timedelta doesn't check if timezone changes
+            .replace(hour=0, minute=0, second=0, microsecond=0)
+        )
+
+        if lower_limit > upper_limit:
+            break
+
+    return weekdays
+
 
 def get_unique_epochs(all_epochs: List[int]) -> List[str]:
     """
-    Returns a list 
+    Returns a list
     """
     # dates = [datetime.fromtimestamp(epoch).strftime("%d-%m-%Y") for epoch in all_epochs]
     dates = [utils.get_finnish_date(epoch) for epoch in all_epochs]
@@ -60,7 +115,6 @@ def valid_table_name(table_name: str) -> bool:
 #     print(valid_table_name(";"))
 #     print(valid_table_name("+"))
 #     print(valid_table_name("-"))
-
 
 
 # if __name__ == "__main__":
