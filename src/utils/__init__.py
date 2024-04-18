16e182ccfa3e01e4f581fc603b98835e691502e5
Kasperi <kasperi.ervasti@gmail.com>
2024-04-18 18:02:27 +0300

    Small change

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,2 @@
-# from .helpers import time_to_epoch, epoch_to_time, get_finnish_time, get_finnish_date, get_finnish_day, get_formatted_finnish_time, *
+# from .helpers import formatted_date_to_epoch, epoch_to_time, get_finnish_time, get_finnish_date, get_finnish_day, get_formatted_finnish_time, *
 from .helpers import *
