0c2140f389b2d389cef81ecf7437766e28d626dc
Kasperi <kasperi.ervasti@gmail.com>
2024-04-18 18:06:02 +0300

    Implemented needed helpers to calculate average weekday graph #2

--- src/database/helpers.py
+++ src/database/helpers.py
@@ -8,45 +8,14 @@ import pytz
 import math
 
 
-
-def calculate_averages(sums: list[int], counts: list[int]) -> list[float]:
-    if len(sums) != len(counts):
-        raise ValueError("The lengths of 'sums' and 'counts' lists must be the same.")
-
-    averages = [sum_val / count_val if count_val != 0 else 0.0 for sum_val, count_val in zip(sums, counts)]
-
-    return averages
-
-
-def get_timestamps(start: int, end: int, interval: int) -> List[int]:
-    """Return all timestamps of intervals that fit into start and end"""
-
-    timestamps = []
-
-    duration = end - start
-    loops = math.floor(duration / interval)
-
-    for i in range(loops):
-        timestamps.append(start + i * interval)
-
-    return timestamps
-
-def are_ints(*args):
-    """True if all given arguments were of type int. False otherwise."""
-    for arg in args:
-        if not isinstance(arg, int):
-            return False
-    return True
-
-
 def calculate_days(
     start: int, end: int, weekday: str, tzinfo=pytz.timezone("Europe/Helsinki")
 ) -> List[tuple[int, int]]:
     """Calculates the start day (inclusive) and end day (exclusive)"""
     weekdays: List[tuple[int, int]] = []
 
-    lower_limit = utils.lower_limit(start, weekday)  # Beginning of first weekday
-    upper_limit = utils.upper_limit(end, weekday)  # End of last weekday
+    lower_limit = _lower_limit(start, weekday)  # Beginning of first weekday
+    upper_limit = _upper_limit(end, weekday)  # End of last weekday
 
     while True:
         start_day = utils.get_localized_datetime(lower_limit).replace(
@@ -73,11 +42,117 @@ def calculate_days(
     return weekdays
 
 
+def calculate_averages(sums: list[int], counts: list[int]) -> list[float]:
+    if len(sums) != len(counts):
+        raise ValueError("The lengths of 'sums' and 'counts' lists must be the same.")
+
+    averages = [
+        sum_val / count_val if count_val != 0 else 0.0
+        for sum_val, count_val in zip(sums, counts)
+    ]
+
+    return averages
+
+
+def calculate_timestamps(start: int, end: int, interval: int) -> List[int]:
+    """Returns a list with all timestamps of intervals that fit into start and end"""
+
+    timestamps = []
+
+    duration = end - start
+    loops = math.floor(duration / interval)
+
+    for i in range(loops):
+        timestamps.append(start + i * interval)
+
+    return timestamps
+
+
+def _lower_limit(start: int, weekday: str) -> int:
+    """
+    Finds the next closest day (can be the given start day) to given start time
+    that matches with the given target weekday.
+
+    Parameters:
+    - start (int): Starting time for search as epoch timestamp in seconds.
+    - weekday (str): Target weekday abbreviation ("mon", "tue", "wed", "thu", "fri", "sat", "sun").
+
+    Returns:
+    - int: Closest target weekday as epoch timestamp in seconds. Time of the day will always be set to 00:00:00.
+
+    """
+    weekdays = {"mon": 0, "tue": 1, "wed": 2, "thu": 3, "fri": 4, "sat": 5, "sun": 6}
+    fin_datetime = utils.get_localized_datetime(start)
+
+    current_weekday = fin_datetime.weekday()
+    target_weekday = weekdays[weekday.lower()]
+
+    days_to_add = (target_weekday - current_weekday) % 7
+
+    # Current weekday is the target weekday
+    if days_to_add == 0:
+        # Return current day but time set to 00:00:00.
+        return fin_datetime.replace(
+            hour=0, minute=0, second=0, microsecond=0
+        ).timestamp()
+
+    # Calculate the weekday
+    lower_limit = fin_datetime + timedelta(days=days_to_add)
+
+    # Set time to 00:00:00
+    lower_limit = lower_limit.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    return int(lower_limit.timestamp())
+
+
+def _upper_limit(end: int, weekday: str) -> int:
+    """
+    Finds the previous closest day (+1 day) to given end time
+    that matches with the given target weekday.
+
+    Parameters:
+    - end (int): Ending time for search as epoch timestamp in seconds.
+    - weekday (str): Target weekday abbreviation ("mon", "tue", "wed", "thu", "fri", "sat", "sun").
+
+    Returns:
+    - int: Closest target weekday(+1 day) as epoch timestamp in seconds. Time of the day will always be set to 00:00:00.
+        Example: If target weekday is Tuesday returns previous Wednesday at 00:00:00. (mon -> tue, tue -> wed, ...)
+    """
+
+    weekdays = {"mon": 0, "tue": 1, "wed": 2, "thu": 3, "fri": 4, "sat": 5, "sun": 6}
+
+    fin_datetime = utils.get_localized_datetime(end)
+
+    current_weekday = fin_datetime.weekday()
+    target_weekday = weekdays[weekday.lower()]
+
+    days_to_subtract = (current_weekday - target_weekday) % 7
+
+    # Current weekday is the target weekday
+    if days_to_subtract == 0:
+        # +1 day
+        fin_datetime = fin_datetime + timedelta(days=1)
+        # Return the day, time set to 00:00:00.
+        return int(
+            fin_datetime.replace(hour=0, minute=0, second=0, microsecond=0).timestamp()
+        )
+
+    # Calculate the datetime of the previous occurrence of the target weekday
+    upper_limit_dt = fin_datetime - timedelta(days=days_to_subtract)
+
+    # Set time to 00:00:00
+    upper_limit_dt = upper_limit_dt.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # +1 day
+    upper_limit_dt = upper_limit_dt + timedelta(days=1)
+
+    return int(upper_limit_dt.timestamp())
+
+
 def get_unique_epochs(all_epochs: List[int]) -> List[str]:
     """
     Returns a list
     """
-    # dates = [datetime.fromtimestamp(epoch).strftime("%d-%m-%Y") for epoch in all_epochs]
     dates = [utils.get_finnish_date(epoch) for epoch in all_epochs]
 
     unique_dates = []
@@ -90,32 +165,9 @@ def get_unique_epochs(all_epochs: List[int]) -> List[str]:
     return unique_dates
 
 
-def valid_table_name(table_name: str) -> bool:
-    """
-    Checks that table_name only uses ascii-letters, underscores,
-    numbers or "äÄöÖåÅ"-letters.
-
-    A-Za-z0-9_äÄöÖåÅ
-    """
-    string.ascii_letters
-    if re.match("^[A-Za-z0-9_äÄöÖåÅ]+$", table_name):
-        return True
-    else:
-        return False
-
-
-# def main():
-#     test = "dsa231sadsad23321____31312321ädadäåqwertyuiopåäölkjhgfdsazxcvbnm"
-#     test2 = "----;;;;2131321DELETE"
-#     print(valid_table_name(test))
-#     print(valid_table_name(test2))
-#     print(valid_table_name(""))
-#     print(valid_table_name("das-"))
-#     print(valid_table_name("löä"))
-#     print(valid_table_name(";"))
-#     print(valid_table_name("+"))
-#     print(valid_table_name("-"))
-
-
-# if __name__ == "__main__":
-#     main()
+def are_ints(*args):
+    """True if all given arguments were of type int. False otherwise."""
+    for arg in args:
+        if not isinstance(arg, int):
+            return False
+    return True
