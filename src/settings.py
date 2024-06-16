ad4334861999c1a37a67cfc4059a11e0075fa30b
Kasperi <kasperi.ervasti@gmail.com>
2024-06-16 19:16:17 +0300

    Small change

--- src/settings.py
+++ src/settings.py
@@ -5,15 +5,12 @@ class Settings():
         # KORJAA! Move default settings later to their own config file
 
         # Default settings
-        self.default_db_name = database.DB_NAME
-        self.default_db_directory = database.DB_DIRECTORY
+        self.default_db_path = database.DB_FILE_PATH
         self.default_ylim = (-0.0001, None)
 
         # Current settings
-        self.db_name = database.DB_NAME
-        self.db_directory = database.DB_DIRECTORY
+        self.db_path = database.DB_FILE_PATH
         self.ylim = (-0.0001, None)
 
-    def get_db_path(self) -> str:
-        return self.db_directory + self.db_name
-
+    def set_path_to_default(self):
+        self.db_path = self.default_db_path
