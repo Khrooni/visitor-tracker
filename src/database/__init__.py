019d0f0998a996719d71f958e4ef23c65cf45614
Kasperi <kasperi.ervasti@gmail.com>
2024-06-16 19:15:00 +0300

    Small change

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,5 @@
 # __all__ = ["database"]
 
-from .db_manager import SQLiteDBManager, DB_NAME, DB_DIRECTORY
+from .db_manager import SQLiteDBManager, DB_NAME, DB_DIRECTORY, DB_FILE_PATH
 # from .old_db_manager import initialize_database, add_data, add_location, add_visitor_activity, get_activity_between, get_all
 
